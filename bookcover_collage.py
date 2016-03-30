b1348545b78081d7465959b6526c6277f64c583b
Small-Star <small_star@gmx.com>
2016-03-30 19:27:27 -0400

    Added functionality to pull all images in title list

--- bookcover_collage.py
+++ bookcover_collage.py
@@ -13,8 +13,11 @@ import bs4
 import shutil
 import string
 import warnings
+import time
+import random
+from PIL import Image
 
-def get_cover_image(title,verbose=True,directory='./coverdir'):
+def get_cover_image(title, verbose=True, directory='./coverdir'):
     #Sample goodreads query URL: http://www.goodreads.com/search?utf8=%E2%9C%93&q=atlas+shrugged&search_type=books
 
     fname = directory + "/" + title + ".jpg"
@@ -32,7 +35,7 @@ def get_cover_image(title,verbose=True,directory='./coverdir'):
     search_url = search_url_beg + title + search_url_end
     
     if verbose:
-        print('Pulling search page for %s : %s' %(title, search_url))
+        print('--- Pulling search page for %s : %s' %(title, search_url))
 
     #Get search page
     res = requests.get(search_url, verify=False)
@@ -44,7 +47,7 @@ def get_cover_image(title,verbose=True,directory='./coverdir'):
     top_result_url = "http://www.goodreads.com" + result
 
     if verbose:
-        print('Pulling book page: %s' %top_result_url)        
+        print('--- Pulling book page: %s' %top_result_url)        
 
     #Get the book's page
     res2 = requests.get(top_result_url, verify=False)
@@ -55,7 +58,7 @@ def get_cover_image(title,verbose=True,directory='./coverdir'):
     cover_img_url = soup2.select('img' '#coverImage')[0].get('src')
 
     if verbose:
-        print('Pulling cover: %s' %cover_img_url)
+        print('--- Pulling cover: %s' %cover_img_url)
 
     #Getting cover image
     res3 = requests.get(cover_img_url, verify=False)
@@ -68,15 +71,50 @@ def get_cover_image(title,verbose=True,directory='./coverdir'):
                 f.write(chunk)
 
         if verbose:
-            print('Image written: %s' %fname)
+            print('---Image written: %s' %fname)
     except IOError:
         print ("Error writing image to %s" %fname)
 
-def get_all_images(title_list,directory='./coverdir'):
+def get_all_images(title_list, verbose=True, directory='./coverdir'):
     #Assumes input is a list of strings; each string being a title of a book
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     for t in title_list:
-        get_cover_image(t)
+        fname = directory + "/" + t + ".jpg"
+        
+        if verbose:
+            print('Checking %s...' %t) 
+        
+        if not os.path.isfile(fname):
+            #Image has not already been obtained
+            #Add in a random delay; unsure if scraping is tolerated
+            time.sleep(random.randrange(1,30))
+            get_cover_image(t, verbose)
+
+        else:
+            #Image already exists
+            print('=== %s exists' %fname)
+            continue
+
+def make_collage(verbose=True, directory='./coverdir'):
+    #Concatenate all images in target directory
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+
+    images = []
+    
+    for f in files:
+        #Filter out all but .jpgs
+        try:
+            if Image.open(directory + "/" + f).format == 'JPEG':
+                images.append(f)
+        except IOError:
+            pass
+        
+    if verbose:
+        print('List of images found: %s' %images)
+
+    ###TODO
+    ###Stick images together
+    ###Cleanup
