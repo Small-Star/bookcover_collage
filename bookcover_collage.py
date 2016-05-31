341638823b6cf9590ca012a9112a7b4d4bdc7c87
Small-Star <small_star@gmx.com>
2016-05-31 18:04:29 -0400

    Added Author matching; bugfixes

--- bookcover_collage.py
+++ bookcover_collage.py
@@ -12,8 +12,10 @@ import time
 import random
 import math
 from PIL import Image
+from difflib import SequenceMatcher
 
-def get_cover_image(title, verbose=True, directory='./coverdir'):
+def get_cover_image(title, author, verbose=True, directory='./coverdir'):
+    #Requires at least title; author is optional
     #Sample goodreads query URL: http://www.goodreads.com/search?utf8=%E2%9C%93&q=atlas+shrugged&search_type=books
 
     fname = directory + "/" + title + ".jpg"
@@ -31,33 +33,62 @@ def get_cover_image(title, verbose=True, directory='./coverdir'):
     search_url = search_url_beg + title + search_url_end
     
     if verbose:
-        print('--- Pulling search page for %s : %s' %(title, search_url))
+        print('--- Pulling search page for %s by %s: %s' %(title, author, search_url))
 
     #Get search page
     res = requests.get(search_url, verify=False)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "html.parser")
 
-    #The first result is probably the one we want
-    result = soup.select('.bookTitle')
+    #Get all of the returned results, and match the titles and authors for similarity
+    t_result = soup.select('.bookTitle')
+    if author != None:
+        a_result = []
+        for t in t_result:
+            #Messy way to pull out the first author per title
+            a = t.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.get_text().split("(",1)[0].split(",",1)[0].rstrip()
+            a_result.append(a)
 
-    if len(result) == 0:
+    if len(t_result) == 0:
         #If no results at all are returned, return no image
+        print('No results found')
         return 0
     
-    result_ck = result[0].get('href')
+    if (len(t_result) != len(a_result)) and author != None:
+        print('Error: Record set returned has uneven numbers of authors and titles')
+        return 0
+
+    similarity_list = []
+
+    #Compare each result with the queried title and author, giving a similarity score
+    for r in range(len(t_result)):
+        sim = SequenceMatcher(None, title.lower(), t_result[r].get_text().lstrip().rstrip().lower()).ratio()
+        if author != None:
+            sim += SequenceMatcher(None, author.lower(), a_result[r].lstrip().rstrip().lower()).ratio()
+        similarity_list.append(sim) 
+
+    #The result we want is probably the one with the highest similarity index
+    most_similar_index = similarity_list.index(max(similarity_list))
+    result_ck = t_result[most_similar_index].get('href')
     top_result_url = "http://www.goodreads.com" + result_ck
 
     if verbose:
         print('--- Pulling book page: %s' %top_result_url)        
-
+        print('------ Title: %s' %t_result[most_similar_index].get_text())
+        if author != None:
+            print('------ Author: %s' %a_result[most_similar_index])
+            
     #Get the book's page
     res2 = requests.get(top_result_url, verify=False)
     res2.raise_for_status()
     soup2 = bs4.BeautifulSoup(res2.text, "html.parser")
 
     #Select the cover image <img id="coverImage" src="XXX"
-    cover_img_url = soup2.select('img' '#coverImage')[0].get('src')
+    cover_img_url_t = soup2.select('img' '#coverImage')
+    if len(cover_img_url_t) == 0:
+        return 0
+    else:
+        cover_img_url = cover_img_url_t[0].get('src')
 
     if verbose:
         print('--- Pulling cover: %s' %cover_img_url)
@@ -74,32 +105,45 @@ def get_cover_image(title, verbose=True, directory='./coverdir'):
 
         if verbose:
             print('---Image written: %s' %fname)
+        return 1
     except IOError:
         print ("Error writing image to %s" %fname)
 
-def get_all_images(title_list, verbose=True, directory='./coverdir', rand_delay=True):
+def get_all_images(title_list, author_list, verbose=True, directory='./coverdir', rand_delay=True):
     #Assumes input is a list of strings; each string being a title of a book
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    for t in title_list:
-        fname = directory + "/" + t + ".jpg"
+    num_missing = 0
+    
+    for t in range(len(title_list)):
+        fname = directory + "/" + title_list[t] + ".jpg"
         
         if verbose:
-            print('Checking %s...' %t) 
-        
+            print('Checking %s...' %title_list[t]) 
+            
         if not os.path.isfile(fname):
             #Image has not already been obtained
+
+            #Skip this one if it is a short story - it won't be on Goodreads
+            if title_list[t].find("[Short Story]") != -1:
+                num_missing += 1
+                continue
+            
             #Add in a random delay; unsure if scraping is tolerated
             if rand_delay:
                 time.sleep(random.randrange(1,30))
-            get_cover_image(t, verbose)
+
+            r = get_cover_image(title_list[t], author_list[t], verbose=verbose, directory=directory)
+            if r != 1:
+                num_missing += 1
 
         else:
             #Image already exists
             print('=== %s exists' %fname)
             continue
+    print("Number of images that could not be found: %s" %num_missing)
 
 def make_collage(width=10,verbose=True, directory='./coverdir', output_filename="collage.jpg"):
     #Concatenate all images in target directory
